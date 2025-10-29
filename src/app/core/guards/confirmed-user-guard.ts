fc3cfae64acc705e0b48be19290f4b8aba5ad55e
Gabriel <gabrielp_lacerda@hotmail.com>
2025-10-29 16:46:05 -0300

    refactor: replace CognitoService with AuthService in ConfirmedUserGuard

--- src/app/core/guards/confirmed-user-guard.ts
+++ src/app/core/guards/confirmed-user-guard.ts
@@ -1,14 +1,17 @@
+import { AuthService } from '@core/pages/auth/services/auth.service';
 import { inject, Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
-import { CognitoService } from "@shared/services/cognito/cognito.service";
+import { firstValueFrom, filter, take } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ConfirmedUserGuard implements CanActivate {
   private router = inject(Router);
-  private cognitoService = inject(CognitoService);
+  private authService = inject(AuthService);
 
   async canActivate(): Promise<boolean> {
-    const user = await this.cognitoService.getCurrentUser();
+    // const user = await this.cognitoService.getCurrentUser();
+
+    const user = this.authService.user();
     if (!user?.email_verified) {
       this.router.navigateByUrl('/auth/confirm');
       return false;
